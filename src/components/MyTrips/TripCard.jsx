ba5c6feaeb91bce724ffda90ce3d31c44827eccb
Raihan Uddin <uddinraihan797@gmail.com>
2024-11-02 03:56:46 +0600

    add delete button

--- src/components/MyTrips/TripCard.jsx
+++ src/components/MyTrips/TripCard.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { Badge } from "../ui/badge";
 
+import { MdDeleteForever } from "react-icons/md";
 /* eslint-disable react/prop-types */
 export default function TripCard({ trip }) {
   const { duration, traveler_type } = trip.tripData;
@@ -8,10 +9,17 @@ export default function TripCard({ trip }) {
   const destination = trip.userSelection.location.properties.formatted;
   return (
     <Link to={`/view-trip/${trip.id}`}>
-      <div className="text-black h-full p-4 flex flex-col gap-4 rounded-lg max-w-[350px] hover:bg-[#DBFF73] cursor-pointer bg-white shadow-lg hover:scale-105 transition-all">
-        <h2 className="font-bold text-[20px] text-gray-700 leading-tight">
-          {destination}
-        </h2>
+      <div className="text-black h-full p-4 flex flex-col gap-4 rounded-lg max-w-[350px] hover:bg-[#DBFF73] cursor-pointer bg-white shadow-lg ">
+        <div className="flex gap-2 justify-between">
+          <h2 className="font-bold text-[20px] text-gray-700 leading-tight">
+            {destination}
+          </h2>
+          <div>
+            <button className="p-1 rounded-lg">
+              <MdDeleteForever className="w-5 h-5" />
+            </button>
+          </div>
+        </div>
         <div className="flex flex-row flex-wrap gap-3 text-sm font-normal antialiased ">
           <Badge variant="secondary" className="bg-green-100 text-gray-500">
             {traveler_type}
